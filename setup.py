5685a6b75dec902923f9e4f9fe1a058aac2a3024
CdeBeer7th <coenraad.debeer@gmail.com>
2024-03-11 16:59:43 +0200

    modified:   setup.py

--- setup.py
+++ setup.py
@@ -8,13 +8,13 @@ Author: Coenraad de Beer
 from setuptools import setup
 
 setup(
-    name="install",
+    name="install_scripts",
     version="0.1",
     description="Experimental data automated sync",
     url="https://github.com/CdeBeer7th/labnexus_pyprobe",
     author="de Beer, Coenraad",
     author_email="coenraad.debeer@gmail.com",
-    packages=setuptools.find_packages(),
+    packages=['main'],
     install_requires=[
         "argparse",
         "datetime",
