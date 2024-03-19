db3b643e762a74f9ab374c33612ef28e5b499442
CdeBeer7th <coenraad.debeer@gmail.com>
2024-03-19 17:22:55 +0200

    w

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup
 
 setup(
     name="labnexus_pyprobe",
-    version="0.1",
+    version="0.2",
     description="Experimental data automated sync",
     author="de Beer, Coenraad",
     author_email="coenraad.debeer@gmail.com",
